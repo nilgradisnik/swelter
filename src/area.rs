b907155321397216164daa5ca18d2695ac6e5751
nilgradisnik <nil.gradisnik@gmail.com>
2022-02-16 22:23:49 -0500

    Chart with history

--- src/area.rs
+++ src/area.rs
@@ -2,25 +2,21 @@ use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::gio;
 use gtk::glib;
-use glib::{ParamFlags, ParamSpec, ParamSpecInt, ParamSpecFloat, Value};
+use gtk::glib::clone;
 
-use once_cell::sync::Lazy;
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
 
 mod imp {
     use super::*;
 
-    #[derive(Debug)]
     pub struct SwelterChartArea {
-        pub tick: Cell<i32>,
-        pub temperature: Cell<f32>,
+        pub temperatures: RefCell<Vec<f32>>,
     }
 
     impl Default for SwelterChartArea {
         fn default() -> SwelterChartArea {
             SwelterChartArea {
-                tick: Cell::new(0),
-                temperature: Cell::new(0.0),
+                temperatures: RefCell::new(vec![]),
             }
         }
     }
@@ -38,11 +34,12 @@ mod imp {
 
             let matrix = cairo::Matrix::new(1.0, 0.0, 0.0, -1.0, 0.0, 20.0);
 
-            obj.set_draw_func(move |area, ctx, _, _| {
-                let tick = area.property::<i32>("tick");
-                let temperature = area.property::<f32>("temperature");
+            obj.set_draw_func(clone!(@weak obj => move |_, ctx, _, _| {
+                let imp = obj.imp();
 
-                println!("Tick {:?} Temp: {:?}", tick, temperature);
+                let temperatures = imp.temperatures.borrow();
+
+                println!("Temps: {:?}", temperatures);
 
                 let context = ctx.to_owned();
 
@@ -55,62 +52,19 @@ mod imp {
                 context.paint().expect("Unable to draw on context");
 
                 // Draw chart
-                context.set_source_rgb(1.0, 0.0, 0.0);
-                context.set_line_width(0.1);
+                for (index, temperature) in temperatures.iter().enumerate() {
+                    context.set_source_rgb(1.0, 0.0, 0.0);
+                    context.set_line_width(0.1);
 
-                let x = 19.5 - (tick as f32 / 10.0);
-                let y = temperature / 100.0;
+                    let x = 19.5 - (index as f32 / 10.0);
+                    let y = temperature / 100.0;
 
-                context.move_to(x.into(), 0.0);
-                context.line_to(x.into(), y.into());
+                    context.move_to(x.into(), 0.0);
+                    context.line_to(x.into(), y.into());
 
-                context.stroke().expect("Unable to draw on context");
-            });
-        }
-
-        fn properties() -> &'static [ParamSpec] {
-            static PROPERTIES: Lazy<Vec<ParamSpec>> = Lazy::new(|| {
-                vec![ParamSpecInt::new(
-                    "tick",
-                    "tick",
-                    "tick",
-                    i32::MIN,
-                    i32::MAX,
-                    0,
-                    ParamFlags::READWRITE,
-                ), ParamSpecFloat::new(
-                    "temperature",
-                    "temperature",
-                    "temperature",
-                    f32::MIN,
-                    f32::MAX,
-                    0.0,
-                    ParamFlags::READWRITE,
-                )]
-            });
-            PROPERTIES.as_ref()
-        }
-
-        fn set_property(&self, _obj: &Self::Type, _id: usize, value: &Value, pspec: &ParamSpec) {
-            match pspec.name() {
-                "tick" => {
-                    let value = value.get().expect("The value needs to be of type `i32`.");
-                    self.tick.replace(value);
+                    context.stroke().expect("Unable to draw on context");
                 }
-                "temperature" => {
-                    let value = value.get().expect("The value needs to be of type `f32`.");
-                    self.temperature.replace(value);
-                }
-                _ => unimplemented!(),
-            }
-        }
-
-        fn property(&self, _obj: &Self::Type, _id: usize, pspec: &ParamSpec) -> Value {
-            match pspec.name() {
-                "tick" => self.tick.get().to_value(),
-                "temperature" => self.temperature.get().to_value(),
-                _ => unimplemented!(),
-            }
+            }));
         }
     }
 
@@ -134,11 +88,10 @@ impl SwelterChartArea {
     }
 
     pub fn update_temperature(&self, temperature: f32) {
-        let mut tick = self.property::<i32>("tick");
-        tick += 1;
-        self.set_property("tick", &tick);
+        let imp = self.imp();
 
-        self.set_property("temperature", &temperature);
+        let mut temperatures = imp.temperatures.borrow_mut();
+        temperatures.push(temperature);
 
         self.queue_draw();
     }
